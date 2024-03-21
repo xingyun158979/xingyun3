948b834e00577e5aabdc96cdf4db900a35f40b55
xingyun <382742279@qq.com>
2024-03-21 20:33:13 +0800

    sixth

---
+++ production_system/src/test/java/InfixExpression_test.java
@@ -0,0 +1,27 @@
+import com.project.InfixExpression;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfixExpression_test {
+
+    @Test
+    public void Infix_test()
+    {
+        List<String> expressions = new ArrayList<>();
+        expressions.add("(");
+        expressions.add("3/8");
+        expressions.add("+");
+        expressions.add("1'1/8");
+        expressions.add(")");
+        expressions.add("×");
+        expressions.add("3");
+
+        List<String> houZui = InfixExpression.getSuffixExpressions(expressions);
+        System.out.println(houZui);
+        String result =InfixExpression.resultCal(houZui);
+        System.out.println(result);
+    }
+
+}
